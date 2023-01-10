14575a3a11acc62135356f2bbc99c37c6edc68b0
Jake Norton <jake.norton88@gmail.com>
2023-01-10 14:14:19 +1300

    fixing repo

--- semantic_parsing.py
+++ semantic_parsing.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e83278b5a51428f10b160edc99754c9b070665f645ee068103f2be8d77ddc3a6
-size 9535
+oid sha256:76ebd4e0a4b65202817d6a1d22b8f49edefbe0923d648038a515d429966dad9a
+size 11915
